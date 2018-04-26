eeac1dc66f05f0cfd1b9add3dabcd72eb6ee5d01
Takuo Watanabe <takuo@acm.org>
2018-04-27 07:24:04 +0900

    1st working version

--- app/src/main/java/jp/ac/titech/itpro/sdl/activitytest3/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/activitytest3/MainActivity.java
@@ -1,13 +1,89 @@
 package jp.ac.titech.itpro.sdl.activitytest3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements View.OnClickListener {
+    private final static String TAG = "MainActivity";
+    private final static String ACTION_NAME = "jp.ac.titech.itpro.sdl.ACTION_NAME";
+    private final static int REQ_NAME = 1234;
+
+    private TextView answerText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
         setContentView(R.layout.activity_main);
+        TextView tagText = findViewById(R.id.main_tag_text);
+        tagText.setText(TAG);
+        answerText = findViewById(R.id.answer_text);
+        Button goButton = findViewById(R.id.go_button);
+        goButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Log.d(TAG, "onClick");
+        switch (v.getId()) {
+        case R.id.go_button:
+            Intent intent = new Intent(ACTION_NAME);
+            startActivityForResult(intent, REQ_NAME);
+            break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int reqCode, int resCode, Intent data) {
+        Log.d(TAG, "onActivityResult");
+        switch (reqCode) {
+        case REQ_NAME:
+            if (resCode == RESULT_OK) {
+                String name = data.getStringExtra("name");
+                if (name != null && name.length() > 0) {
+                    answerText.setText(getString(R.string.answer_text_format, name));
+                }
+            }
+            else {
+                answerText.setText(R.string.answer_text_default);
+            }
+            break;
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
     }
 }
